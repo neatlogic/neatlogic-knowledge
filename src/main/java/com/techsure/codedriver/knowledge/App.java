35c9eb63e59dac9ac9bf72a6dcbff5217dae9cac
吕佐康 <lvzk@techsure.com.cn>
2020-10-12 11:17:51 +0800

    commit

---
+++ src/main/java/com/techsure/codedriver/knowledge/App.java
@@ -0,0 +1,13 @@
+package com.techsure.codedriver.knowledge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

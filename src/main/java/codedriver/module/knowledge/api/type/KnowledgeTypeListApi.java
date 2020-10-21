97c4b0cf86bad43e1e1e991faba50296960a2fd7
林邦泉 <linbq@techsure.com.cn>
2020-10-21 16:56:05 +0800

    --story=1003454 --user=林邦泉 知识库编辑页交互-后端 https://www.tapd.cn/54247054/s/1098799

--- src/main/java/codedriver/module/knowledge/api/type/KnowledgeTypeListApi.java
+++ src/main/java/codedriver/module/knowledge/api/type/KnowledgeTypeListApi.java
@@ -1,16 +1,35 @@
 package codedriver.module.knowledge.api.type;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson.JSONObject;
 
+import codedriver.framework.asynchronization.threadlocal.UserContext;
+import codedriver.framework.common.constvalue.GroupSearch;
 import codedriver.framework.reminder.core.OperationTypeEnum;
+import codedriver.framework.restful.annotation.Description;
 import codedriver.framework.restful.annotation.OperationType;
+import codedriver.framework.restful.annotation.Output;
+import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+import codedriver.module.knowledge.dao.mapper.KnowledgeCircleMapper;
+import codedriver.module.knowledge.dao.mapper.KnowledgeDocumentMapper;
+import codedriver.module.knowledge.dto.KnowledgeCircleUserVo;
+import codedriver.module.knowledge.dto.KnowledgeDocumentVersionVo;
+import codedriver.module.knowledge.dto.KnowledgeTypeVo2;
 
 @Service
 @OperationType(type = OperationTypeEnum.SEARCH)
 public class KnowledgeTypeListApi extends PrivateApiComponentBase {
+
+    @Autowired
+    private KnowledgeDocumentMapper knowledgeDocumentMapper;
+    
+    @Autowired
+    private KnowledgeCircleMapper knowledgeCircleMapper;
 
     @Override
     public String getToken() {
@@ -27,9 +46,23 @@ public class KnowledgeTypeListApi extends PrivateApiComponentBase {
         return null;
     }
 
+    @Output({
+        @Param(explode = KnowledgeTypeVo2[].class, desc = "知识分类列表")
+    })
+    @Description(desc = "查询知识分类列表")
     @Override
     public Object myDoService(JSONObject jsonObj) throws Exception {
-        // TODO Auto-generated method stub
+//        int isReviewable = 0;
+//        List<KnowledgeCircleUserVo> knowledgeCircleUserList = knowledgeCircleMapper.getKnowledgeCircleUserListByIdAndAuthType(knowledgeDocumentVo.getKnowledgeCircleId(), KnowledgeCircleUserVo.AuthType.APPROVER.getValue());
+//        for(KnowledgeCircleUserVo knowledgeCircleUserVo : knowledgeCircleUserList) {
+//            if(GroupSearch.USER.getValue().equals(knowledgeCircleUserVo.getType())) {
+//               if(UserContext.get().getUserUuid(true).equals(knowledgeCircleUserVo.getUuid())) {
+//                   isReviewable = 1;
+//               }
+//            }
+//        }
+//        
+//        int count = knowledgeDocumentMapper.getKnowledgeDocumentVersionMyVersionCount(new KnowledgeDocumentVersionVo());
         return null;
     }
 

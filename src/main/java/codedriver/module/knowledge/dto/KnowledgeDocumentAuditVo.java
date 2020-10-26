e597c016200d860789e608f1036c84866420a011
林邦泉 <linbq@techsure.com.cn>
2020-10-26 19:28:54 +0800

    --story=1003454 --user=林邦泉 知识库编辑页交互-后端 https://www.tapd.cn/54247054/s/1100133

---
+++ src/main/java/codedriver/module/knowledge/dto/KnowledgeDocumentAuditVo.java
@@ -0,0 +1,56 @@
+package codedriver.module.knowledge.dto;
+
+import com.alibaba.fastjson.JSONObject;
+
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.restful.annotation.EntityField;
+
+public class KnowledgeDocumentAuditVo {
+    
+    @EntityField(name = "文档id", type = ApiParamType.LONG)
+    private Long KnowledgeDocumentId;
+    @EntityField(name = "操作类型", type = ApiParamType.STRING)
+    private String operate;
+    @EntityField(name = "操作描述", type = ApiParamType.STRING)
+    private String title;
+    @EntityField(name = "内容", type = ApiParamType.STRING)
+    private String content;
+    private transient JSONObject config;
+    private transient String configHash;
+    public Long getKnowledgeDocumentId() {
+        return KnowledgeDocumentId;
+    }
+    public void setKnowledgeDocumentId(Long knowledgeDocumentId) {
+        KnowledgeDocumentId = knowledgeDocumentId;
+    }
+    public String getOperate() {
+        return operate;
+    }
+    public void setOperate(String operate) {
+        this.operate = operate;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public JSONObject getConfig() {
+        return config;
+    }
+    public void setConfig(JSONObject config) {
+        this.config = config;
+    }
+    public String getConfigHash() {
+        return configHash;
+    }
+    public void setConfigHash(String configHash) {
+        this.configHash = configHash;
+    }
+}

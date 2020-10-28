3cdf139455b2e36538eed1cfc675088f552a7b98
林邦泉 <linbq@techsure.com.cn>
2020-10-28 11:41:49 +0800

    --story=1003454 --user=林邦泉 知识库编辑页交互-后端 https://www.tapd.cn/54247054/s/1100721

--- src/main/java/codedriver/module/knowledge/service/KnowledgeDocumentServiceImpl.java
+++ src/main/java/codedriver/module/knowledge/service/KnowledgeDocumentServiceImpl.java
@@ -1,19 +1,51 @@
 package codedriver.module.knowledge.service;
 
+import java.util.List;
+
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import codedriver.framework.asynchronization.threadlocal.UserContext;
+import codedriver.framework.dao.mapper.UserMapper;
+import codedriver.framework.dto.UserVo;
+import codedriver.framework.file.dao.mapper.FileMapper;
+import codedriver.framework.file.dto.FileVo;
 import codedriver.module.knowledge.constvalue.KnowledgeDocumentVersionStatus;
+import codedriver.module.knowledge.dao.mapper.KnowledgeCircleMapper;
 import codedriver.module.knowledge.dao.mapper.KnowledgeDocumentMapper;
+import codedriver.module.knowledge.dao.mapper.KnowledgeDocumentTypeMapper;
+import codedriver.module.knowledge.dao.mapper.KnowledgeTagMapper;
+import codedriver.module.knowledge.dto.KnowledgeCircleVo;
+import codedriver.module.knowledge.dto.KnowledgeDocumentFileVo;
+import codedriver.module.knowledge.dto.KnowledgeDocumentLineVo;
+import codedriver.module.knowledge.dto.KnowledgeDocumentTagVo;
+import codedriver.module.knowledge.dto.KnowledgeDocumentTypeVo;
 import codedriver.module.knowledge.dto.KnowledgeDocumentVersionVo;
 import codedriver.module.knowledge.dto.KnowledgeDocumentVo;
+import codedriver.module.knowledge.exception.KnowledgeDocumentNotFoundException;
+import codedriver.module.knowledge.exception.KnowledgeDocumentVersionNotFoundException;
 
 @Service
 public class KnowledgeDocumentServiceImpl implements KnowledgeDocumentService {
 
     @Autowired
     private KnowledgeDocumentMapper knowledgeDocumentMapper;
+    
+    @Autowired
+    private KnowledgeTagMapper knowledgeTagMapper;
+    
+    @Autowired
+    private FileMapper fileMapper;
+
+    @Autowired
+    private KnowledgeDocumentTypeMapper knowledgeDocumentTypeMappper;
+    
+    @Autowired
+    private KnowledgeCircleMapper knowledgeCircleMapper;
+    
+    @Autowired
+    private UserMapper userMapper;
 
     @Override
     public int isDeletable(KnowledgeDocumentVersionVo knowledgeDocumentVersionVo) {
@@ -58,6 +90,54 @@ public class KnowledgeDocumentServiceImpl implements KnowledgeDocumentService {
             }
         }
         return 0;
+    }
+
+    @Override
+    public KnowledgeDocumentVo getKnowledgeDocumentDetailByKnowledgeDocumentVersionId(Long knowledgeDocumentVersionId) {
+        KnowledgeDocumentVersionVo knowledgeDocumentVersionVo = knowledgeDocumentMapper.getKnowledgeDocumentVersionById(knowledgeDocumentVersionId);
+        if(knowledgeDocumentVersionVo == null) {
+            throw new KnowledgeDocumentVersionNotFoundException(knowledgeDocumentVersionId);
+        }
+        KnowledgeDocumentVo knowledgeDocumentVo = knowledgeDocumentMapper.getKnowledgeDocumentById(knowledgeDocumentVersionVo.getKnowledgeDocumentId());
+        if(knowledgeDocumentVo == null) {
+            throw new KnowledgeDocumentNotFoundException(knowledgeDocumentVersionVo.getKnowledgeDocumentId());
+        }else {
+            knowledgeDocumentVo.setKnowledgeDocumentVersionId(knowledgeDocumentVersionId);
+        }
+        knowledgeDocumentVo.setTitle(knowledgeDocumentVersionVo.getTitle());
+        knowledgeDocumentVo.setKnowledgeDocumentTypeUuid(knowledgeDocumentVersionVo.getKnowledgeDocumentTypeUuid());
+        knowledgeDocumentVo.setLcu(knowledgeDocumentVersionVo.getLcu());
+        UserVo lcuUserVo = userMapper.getUserBaseInfoByUuid(knowledgeDocumentVersionVo.getLcu());
+        if(lcuUserVo != null) {
+            knowledgeDocumentVo.setLcuName(lcuUserVo.getUserName());
+            knowledgeDocumentVo.setLcuInfo(lcuUserVo.getUserInfo());
+        }
+        List<KnowledgeDocumentLineVo> lineList = knowledgeDocumentMapper.getKnowledgeDocumentLineListByKnowledgeDocumentVersionId(knowledgeDocumentVersionId);
+        knowledgeDocumentVo.setLineList(lineList);
+        List<Long> fileIdList = knowledgeDocumentMapper.getKnowledgeDocumentFileIdListByKnowledgeDocumentIdAndVersionId(new KnowledgeDocumentFileVo(knowledgeDocumentVo.getId(), knowledgeDocumentVersionId));
+        if(CollectionUtils.isNotEmpty(fileIdList)) {
+            List<FileVo> fileList = fileMapper.getFileListByIdList(fileIdList);
+            knowledgeDocumentVo.setFileIdList(fileIdList);
+            knowledgeDocumentVo.setFileList(fileList);
+        }
+        List<Long> tagIdList = knowledgeDocumentMapper.getKnowledgeDocumentTagIdListByKnowledgeDocumentIdAndVersionId(new KnowledgeDocumentTagVo(knowledgeDocumentVo.getId(), knowledgeDocumentVersionId));
+        if(CollectionUtils.isNotEmpty(tagIdList)) {
+            List<String> tagNameList = knowledgeTagMapper.getKnowledgeTagNameListByIdList(tagIdList);
+            knowledgeDocumentVo.setTagList(tagNameList);
+        }
+        
+        KnowledgeCircleVo knowledgeCircleVo = knowledgeCircleMapper.getKnowledgeCircleById(knowledgeDocumentVo.getKnowledgeCircleId());
+        if(knowledgeCircleVo != null) {
+            knowledgeDocumentVo.getPath().add(knowledgeCircleVo.getName());
+        }
+        KnowledgeDocumentTypeVo knowledgeDocumentTypeVo = knowledgeDocumentTypeMappper.getTypeByUuid(knowledgeDocumentVo.getKnowledgeDocumentTypeUuid());
+        if(knowledgeDocumentTypeVo != null) {
+            List<String> typeNameList = knowledgeDocumentTypeMappper.getAncestorsAndSelfNameByLftRht(knowledgeDocumentTypeVo.getLft(), knowledgeDocumentTypeVo.getRht(), knowledgeDocumentTypeVo.getKnowledgeCircleId());
+            if(CollectionUtils.isNotEmpty(typeNameList)) {
+                knowledgeDocumentVo.getPath().addAll(typeNameList);
+            }
+        }
+        return knowledgeDocumentVo;
     }
 
 }

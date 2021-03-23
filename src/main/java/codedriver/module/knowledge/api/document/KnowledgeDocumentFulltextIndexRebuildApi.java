48f7c4d21cd99739a8e793c630eb79da9ea3e498
吕佐康 <lvzk@techsure.com.cn>
2021-03-23 20:14:22 +0800

    知识库版本批量重建索引接口（处理历史数据）

---
+++ src/main/java/codedriver/module/knowledge/api/document/KnowledgeDocumentFulltextIndexRebuildApi.java
@@ -0,0 +1,67 @@
+package codedriver.module.knowledge.api.document;
+
+import codedriver.framework.auth.core.AuthAction;
+import codedriver.framework.auth.label.NO_AUTH;
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.fulltextindex.core.FullTextIndexHandlerFactory;
+import codedriver.framework.fulltextindex.core.IFullTextIndexHandler;
+import codedriver.framework.restful.annotation.Input;
+import codedriver.framework.restful.annotation.OperationType;
+import codedriver.framework.restful.annotation.Param;
+import codedriver.framework.restful.constvalue.OperationTypeEnum;
+import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+import codedriver.module.knowledge.dao.mapper.KnowledgeDocumentMapper;
+import codedriver.module.knowledge.fulltextindex.FullTextIndexType;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author lvzk
+ * @since 2021/3/23 17:23
+ **/
+@Service
+@OperationType(type = OperationTypeEnum.UPDATE)
+@AuthAction(action = NO_AUTH.class)
+public class KnowledgeDocumentFulltextIndexRebuildApi extends PrivateApiComponentBase {
+    @Resource
+    KnowledgeDocumentMapper knowledgeDocumentMapper;
+
+    @Override
+    public String getName() {
+        return "重建知识版本索引";
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+    @Input({@Param(name = "versionIdList", type = ApiParamType.JSONARRAY, desc = "版本号idList") })
+    @Override
+    public Object myDoService(JSONObject jsonObj) throws Exception {
+        JSONArray versionIdArray = jsonObj.getJSONArray("versionIdList");
+        List<Long> versionIdList = null;
+        //创建全文检索索引
+        IFullTextIndexHandler handler = FullTextIndexHandlerFactory.getComponent(FullTextIndexType.KNOW_DOCUMENT_VERSION);
+        if (handler != null) {
+            if(CollectionUtils.isNotEmpty(versionIdArray)){
+                versionIdList = JSONObject.parseArray(versionIdArray.toJSONString(), Long.class);
+            }else{
+                versionIdList = knowledgeDocumentMapper.getKnowledgeDocumentVersionIdList();
+            }
+            for(Long versionIdObj : versionIdList ){
+                handler.createIndex(versionIdObj);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String getToken() {
+        return "/knowledge/document/version/fulltext/index/rebuild";
+    }
+}

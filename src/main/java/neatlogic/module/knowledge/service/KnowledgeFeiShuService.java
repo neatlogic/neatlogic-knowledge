60bcce7ffb3e7c2a067617f30cac43644aaed6ea
1437892690@qq.com <1437892690@qq.com>
2026-06-07 17:34:38 +0800

    [功能] 飞书文档导入 neatlogic 知识库
    关联 #[1695041165459459]飞书文档导入 neatlogic 知识库 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1695041165459459

--- src/main/java/neatlogic/module/knowledge/service/KnowledgeFeiShuService.java
+++ src/main/java/neatlogic/module/knowledge/service/KnowledgeFeiShuService.java
@@ -1,8 +1,20 @@
 package neatlogic.module.knowledge.service;
 
+import neatlogic.framework.knowledge.dto.KnowledgeDocumentTypeVo;
 import neatlogic.framework.knowledge.dto.feishu.FeiShuAppCredentialsVo;
+import neatlogic.framework.knowledge.dto.feishu.FeiShuNodeVo;
+
+import java.util.List;
 
 public interface KnowledgeFeiShuService {
 
     FeiShuAppCredentialsVo getFeiShuAppCredentials();
+
+    KnowledgeDocumentTypeVo getOrCreateKnowledgeType(String name, String parentUuid, Long knowledgeCircleId);
+
+    List<FeiShuNodeVo> loadWikiNodes(Long spaceId, String parentNodeToken, List<String> path, String tenantAccessToken);
+
+    void saveNodes(List<FeiShuNodeVo> nodes, FeiShuAppCredentialsVo config, KnowledgeDocumentTypeVo knowledgeType, String tenantAccessToken);
+
+    void saveFeiShuDocument(FeiShuAppCredentialsVo appCredentialsVo, FeiShuNodeVo node, String typeUuid, String tenantAccessToken);
 }

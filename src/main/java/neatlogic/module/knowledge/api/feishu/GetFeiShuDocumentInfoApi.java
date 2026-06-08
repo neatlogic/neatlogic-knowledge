cb5f46bace67aef9fcbfdb91a60d5d9dcbf5ac0c
1437892690@qq.com <1437892690@qq.com>
2026-06-08 12:10:06 +0800

    [功能] 飞书文档导入 neatlogic 知识库
    关联 #[1695041165459459]飞书文档导入 neatlogic 知识库 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1695041165459459

--- src/main/java/neatlogic/module/knowledge/api/feishu/GetFeiShuDocumentInfoApi.java
+++ src/main/java/neatlogic/module/knowledge/api/feishu/GetFeiShuDocumentInfoApi.java
@@ -13,6 +13,8 @@ package neatlogic.module.knowledge.api.feishu;
 import com.alibaba.fastjson.JSONObject;
 import neatlogic.framework.auth.core.AuthAction;
 import neatlogic.framework.common.constvalue.ApiParamType;
+import neatlogic.framework.knowledge.dto.feishu.FeiShuAppCredentialsVo;
+import neatlogic.framework.knowledge.dto.feishu.FeiShuNodeVo;
 import neatlogic.framework.restful.annotation.Description;
 import neatlogic.framework.restful.annotation.Input;
 import neatlogic.framework.restful.annotation.OperationType;
@@ -20,12 +22,20 @@ import neatlogic.framework.restful.annotation.Param;
 import neatlogic.framework.restful.constvalue.OperationTypeEnum;
 import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
 import neatlogic.module.knowledge.auth.label.KNOWLEDGE_FEISHU_SYNC_MODIFY;
+import neatlogic.module.knowledge.service.KnowledgeFeiShuService;
+import neatlogic.module.knowledge.utils.FeiShuOpenApiUtil;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 @Service
 @AuthAction(action = KNOWLEDGE_FEISHU_SYNC_MODIFY.class)
 @OperationType(type = OperationTypeEnum.SEARCH)
 public class GetFeiShuDocumentInfoApi extends PrivateApiComponentBase {
+    @Resource
+    private KnowledgeFeiShuService knowledgeFeiShuService;
 
     @Override
     public String getToken() {
@@ -44,6 +54,25 @@ public class GetFeiShuDocumentInfoApi extends PrivateApiComponentBase {
     @Override
     public Object myDoService(JSONObject paramObj) throws Exception {
         String nodeToken = paramObj.getString("nodeToken");
+        FeiShuAppCredentialsVo feiShuAppCredentials = knowledgeFeiShuService.getFeiShuAppCredentials();
+        if (feiShuAppCredentials == null) {
+            return null;
+        }
+        String tenantAccessToken = FeiShuOpenApiUtil.getTenantAccessToken(feiShuAppCredentials.getAppId(), feiShuAppCredentials.getAppSecret());
+        if (StringUtils.isBlank(tenantAccessToken)) {
+            return null;
+        }
+        JSONObject feishuNodeInfo = FeiShuOpenApiUtil.getFeiShuNodeInfo(nodeToken, tenantAccessToken);
+        if (feishuNodeInfo != null) {
+            JSONObject data = feishuNodeInfo.getJSONObject("data");
+            if (MapUtils.isNotEmpty(data)) {
+                JSONObject node = data.getJSONObject("node");
+                if (MapUtils.isNotEmpty(node)) {
+                    FeiShuNodeVo feiShuNodeVo = new FeiShuNodeVo(node);
+                    return FeiShuOpenApiUtil.getDocumentBlocks(feiShuNodeVo.getObjToken(), tenantAccessToken);
+                }
+            }
+        }
         return null;
     }
 }

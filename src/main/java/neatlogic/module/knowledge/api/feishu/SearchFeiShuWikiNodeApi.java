3e632ab86cabae7acbe72d408945ff9b5fdd7151
1437892690@qq.com <1437892690@qq.com>
2026-06-08 10:59:12 +0800

    [功能] 飞书文档导入 neatlogic 知识库
    关联 #[1695041165459459]飞书文档导入 neatlogic 知识库 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1695041165459459

--- src/main/java/neatlogic/module/knowledge/api/feishu/SearchFeiShuWikiNodeApi.java
+++ src/main/java/neatlogic/module/knowledge/api/feishu/SearchFeiShuWikiNodeApi.java
@@ -2,12 +2,14 @@ package neatlogic.module.knowledge.api.feishu;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.oceanbase.jdbc.internal.util.StringCacheUtil;
 import neatlogic.framework.asynchronization.threadlocal.UserContext;
 import neatlogic.framework.auth.core.AuthAction;
 import neatlogic.framework.common.constvalue.ApiParamType;
 import neatlogic.framework.common.dto.BasePageVo;
 import neatlogic.framework.knowledge.constvalue.Status;
 import neatlogic.framework.knowledge.dto.feishu.FeiShuAppCredentialsVo;
+import neatlogic.framework.knowledge.dto.feishu.FeiShuNodeSearchVo;
 import neatlogic.framework.knowledge.dto.feishu.FeiShuNodeVo;
 import neatlogic.framework.knowledge.dto.feishu.KnowledgeFeiShuDocumentMappingVo;
 import neatlogic.framework.restful.annotation.Description;
@@ -22,13 +24,11 @@ import neatlogic.module.knowledge.dao.mapper.KnowledgeFeiShuMapper;
 import neatlogic.module.knowledge.service.KnowledgeFeiShuService;
 import neatlogic.module.knowledge.utils.FeiShuOpenApiUtil;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -45,6 +45,10 @@ public class SearchFeiShuWikiNodeApi extends PrivateApiComponentBase {
     public String getName() { return "获取飞书Wiki节点列表"; }
 
     @Input({
+            @Param(name = "keyword", type = ApiParamType.STRING, desc = "common.keyword"),
+            @Param(name = "currentPage", type = ApiParamType.INTEGER, desc = "common.currentpage"),
+            @Param(name = "pageSize", type = ApiParamType.INTEGER, desc = "common.pagesize"),
+            @Param(name = "status", type = ApiParamType.ENUM, member = Status.class, desc = "common.status"),
             @Param(name = "spaceId", type = ApiParamType.LONG, isRequired = true, desc = "Wiki空间ID"),
             @Param(name = "parentNodeToken", type = ApiParamType.STRING, desc = "父节点nodeToken")
     })
@@ -56,8 +60,23 @@ public class SearchFeiShuWikiNodeApi extends PrivateApiComponentBase {
         if (feiShuAppCredentials == null) {
             return TableResultUtil.getResult(tbodyList);
         }
-        Long spaceId = jsonObj.getLong("spaceId");
-        String parentNodeToken = jsonObj.getString("parentNodeToken");
+        FeiShuNodeSearchVo feiShuNodeSearchVo = jsonObj.toJavaObject(FeiShuNodeSearchVo.class);
+        if (Objects.equals(feiShuNodeSearchVo.getStatus(), Status.SUCCEED.getValue())
+                || Objects.equals(feiShuNodeSearchVo.getStatus(), Status.FAILED.getValue())
+                || Objects.equals(feiShuNodeSearchVo.getStatus(), Status.RUNNING.getValue())
+                || Objects.equals(feiShuNodeSearchVo.getStatus(), Status.WAITING.getValue())
+                || Objects.equals(feiShuNodeSearchVo.getStatus(), Status.UNSUPPORTED.getValue())
+        ) {
+            return searchForDB(feiShuNodeSearchVo, feiShuAppCredentials);
+        } else {
+            return searchForFeiShuOpenApi(feiShuNodeSearchVo, feiShuAppCredentials);
+        }
+    }
+
+    private JSONObject searchForFeiShuOpenApi(FeiShuNodeSearchVo feiShuNodeSearchVo, FeiShuAppCredentialsVo feiShuAppCredentials) {
+        List<KnowledgeFeiShuDocumentMappingVo> tbodyList = new ArrayList<>();
+        Long spaceId = feiShuNodeSearchVo.getSpaceId();
+        String parentNodeToken = feiShuNodeSearchVo.getParentNodeToken();
         String tenantAccessToken = FeiShuOpenApiUtil.getTenantAccessToken(feiShuAppCredentials.getAppId(), feiShuAppCredentials.getAppSecret());
         List<FeiShuNodeVo> feiShuNodeVoList = loadWikiNodes(spaceId, parentNodeToken, new ArrayList<>(), tenantAccessToken);
         if (CollectionUtils.isNotEmpty(feiShuNodeVoList)) {
@@ -98,6 +117,11 @@ public class SearchFeiShuWikiNodeApi extends PrivateApiComponentBase {
         basePageVo.setPageSize(Math.min(pageSize, 100));
         basePageVo.setRowNum(rowNum);
         return TableResultUtil.getResult(tbodyList, basePageVo);
+    }
+
+    private JSONObject searchForDB(FeiShuNodeSearchVo feiShuNodeSearchVo, FeiShuAppCredentialsVo feiShuAppCredentials) {
+
+        return null;
     }
 
     private List<FeiShuNodeVo> loadWikiNodes(Long spaceId, String parentNodeToken, List<String> path, String tenantAccessToken) {
